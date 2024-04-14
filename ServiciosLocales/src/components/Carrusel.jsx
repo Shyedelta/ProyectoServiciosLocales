c0fa865115d675b0417c4bc7c50c3fdb7c575a07
Shyedelta <shirodelta@gmail.com>
2024-04-14 14:50:49 +0200

    Quitando modal

--- ServiciosLocales/src/components/Carrusel.jsx
+++ ServiciosLocales/src/components/Carrusel.jsx
@@ -51,12 +51,12 @@ function Carrusel({ coords, setCoords }) {
           {[...Array(10)].map((_, index) => (
             <div key={index} className="animate-pulse">
               <div className="m-2 bg-purple-100 p-5 my-5 shadow-lg rounded-3xl aspect-square h-72 w-72 flex justify-center align-middle">
-                <div class="loader w-max h-40 pt-32 ">
-                  <div class="loader__circle"></div>
-                  <div class="loader__circle"></div>
-                  <div class="loader__circle"></div>
-                  <div class="loader__circle"></div>
-                  <div class="loader__circle"></div>
+                <div className="loader w-max h-40 pt-32 ">
+                  <div className="loader__circle"></div>
+                  <div className="loader__circle"></div>
+                  <div className="loader__circle"></div>
+                  <div className="loader__circle"></div>
+                  <div className="loader__circle"></div>
                 </div>
               </div>
             </div>
